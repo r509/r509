7dc1c5cb3d8f0a6d0a23393e6048ba857b05d012
Paul Kehrer <paul.l.kehrer@gmail.com>
2011-12-07 11:03:39 -0600

    wrapped ocsp helper classes inside modules
    0 failed! 123 run, 1 pending

--- lib/r509/CertificateStatusChecker.rb
+++ lib/r509/CertificateStatusChecker.rb
@@ -1,12 +1,17 @@
 require 'openssl'
+require 'r509/Ocsp'
 
-class CertificateStatusChecker
-    def initialize(issuer_config)
-        @config = issuer_config
-    end
+module R509::Ocsp
+    module Helper
+        class CertificateStatusChecker
+            def initialize(issuer_config)
+                @config = issuer_config
+            end
 
-    def get_status(certid)
-        #TODO...check anything
-        {:certid=>certid,:status=>OpenSSL::OCSP::V_CERTSTATUS_GOOD,:revocation_reason=>0,:revocation_time=>nil,:config=>@config}
+            def get_status(certid)
+                #TODO...check anything
+                {:certid=>certid,:status=>OpenSSL::OCSP::V_CERTSTATUS_GOOD,:revocation_reason=>0,:revocation_time=>nil,:config=>@config}
+            end
+        end
     end
 end
--- lib/r509/HelperClasses.rb
+++ lib/r509/HelperClasses.rb
@@ -1,15 +1,17 @@
 require 'openssl'
 require 'r509/Config'
 
-class FirstConfigMatch
-    def self.match(certid,configs)
-        configs.each do |config|
-            root_certid = OpenSSL::OCSP::CertificateId.new(config.ca_cert,config.ca_cert)
-            if certid.cmp_issuer(root_certid) then
-                return config
+module R509::Helper
+    class FirstConfigMatch
+        def self.match(certid,configs)
+            configs.each do |config|
+                root_certid = OpenSSL::OCSP::CertificateId.new(config.ca_cert,config.ca_cert)
+                if certid.cmp_issuer(root_certid) then
+                    return config
+                end
             end
+            #if no match return nil
+            nil
         end
-        #if no match return nil
-        nil
     end
 end
