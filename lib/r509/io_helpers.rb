55d831bab28771703a42814c200fd390720066fd
woodbusy <woodbusy@gmail.com>
2012-05-16 14:25:23 -0500

    Avoided error on file.close() calls to nil file

--- lib/r509/io_helpers.rb
+++ lib/r509/io_helpers.rb
@@ -30,7 +30,7 @@ module R509
                     file = File.open(filename_or_io, 'rb:ascii-8bit')
                     return file.read()
                 ensure
-                    file.close()
+                    file.close() unless file.nil?
                 end
             end
         end
